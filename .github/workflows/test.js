7138742f14fa588882b6b02dc89c976a462711ad
Bruce Becker <brucellino@protonmail.com>
2023-04-23 10:02:04 +0200

    test: remove spurious option in test case
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -7,7 +7,6 @@ export const options = {
   thresholds: {
     http_req_failed: ['rate<0.001'], // http errors should be less than 1%
     http_req_duration: ['p(95)<150'], // 95 percent of response times must be below 100ms
-    http_req_duration:
   },
 };
 
